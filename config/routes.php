6fd5ff6e80b30ad0baadcdd2081ce904174296b1
Antony Githendu <antonygithendu@gmail.com>
2024-08-20 07:54:59 +0300

    Extending Views

--- config/routes.php
+++ config/routes.php
@@ -4,11 +4,10 @@ use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
 $routes->setRouteClass(DashedRoute::class);
 $routes->scope('/', function (RouteBuilder $builder) {
-   // Register scoped middleware for in scopes.
    $builder->registerMiddleware('csrf', new CsrfProtectionMiddleware([
       'httponly' => true,
    ]));
    $builder->applyMiddleware('csrf');
-   $builder->connect('template',['controller'=>'Products','action'=>'view']);
+   $builder->connect('extend',['controller'=>'Extends','action'=>'index']);
    $builder->fallbacks();
 });
