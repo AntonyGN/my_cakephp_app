a9418ffdd0d8435da2c9038920e6a7a4ffb578f2
Antony Githendu <antonygithendu@gmail.com>
2024-08-20 06:23:31 +0300

    Generating URLs

--- config/routes.php
+++ config/routes.php
@@ -2,20 +2,13 @@
 use Cake\Http\Middleware\CsrfProtectionMiddleware;
 use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
-
 $routes->setRouteClass(DashedRoute::class);
-
 $routes->scope('/', function (RouteBuilder $builder) {
-    // Register scoped middleware for in scopes.
-    $builder->registerMiddleware('csrf', new CsrfProtectionMiddleware([
-        'httponly' => true,
-    ]));
-    $builder->applyMiddleware('csrf');
-
-    // Update the route placeholders from colon-prefixed to braced
-    $builder->connect('tests/{arg1}/{arg2}', ['controller' => 'Tests', 'action' => 'show'], ['pass' => ['arg1', 'arg2']]);
-
-    $builder->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
-
-    $builder->fallbacks();
+   // Register scoped middleware for in scopes.
+   $builder->registerMiddleware('csrf', new CsrfProtectionMiddleware([
+      'httponly' => true,
+   ]));
+   $builder->applyMiddleware('csrf');
+   $builder->connect('/generate',['controller'=>'Generates','action'=>'show']);
+   $builder->fallbacks();
 });
