3b19e4f8c2ad3be60d5d03affb338fbf1244ba9b
Antony Githendu <antonygithendu@gmail.com>
2024-08-29 16:50:51 +0300

    security

--- config/routes.php
+++ config/routes.php
@@ -16,7 +16,7 @@ $routes->scope('/', function (RouteBuilder $builder) {
     $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
     
     // Connect other routes
-    $builder->connect('cookie/testcookies',['controller'=>'Cookies','action'=>'testCookies']);
+    $builder->connect('login',['controller'=>'Logins','action'=>'index']);
         
     // Fallback routes
     $builder->fallbacks();
