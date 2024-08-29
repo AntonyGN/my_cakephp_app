e57f99a04fdb7f381fd5b6f76861ce4cc2647386
Antony Githendu <antonygithendu@gmail.com>
2024-08-29 14:23:59 +0300

    Internationalization

--- config/routes.php
+++ config/routes.php
@@ -16,8 +16,9 @@ $routes->scope('/', function (RouteBuilder $builder) {
     $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
     
     // Connect other routes
-    $builder->connect('register',['controller'=>'Registrations','action'=>'index']);
-    
+    $builder->connect('locale',
+      ['controller'=>'Localizations','action'=>'index']);
+        
     // Fallback routes
     $builder->fallbacks();
 });
